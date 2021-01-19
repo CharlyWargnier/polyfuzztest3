802d78a23130fb3b7573c7d09da8583a2460a4f2
Charly Wargnier <cwar05@gmail.com>
2021-01-19 20:21:19 +0000

    Add files

---
+++ app.py
@@ -0,0 +1,14 @@
+
+!pip install polyfuzz
+!pip install flair
+
+
+from polyfuzz import PolyFuzz
+from_list = ["https://www.tatielou.co.uk/apples/sadasda", "https://www.tatielou.co.uk/oranges/sadasda"]
+to_list = ["https://www.tatielou.co.uk/apples/", "https://www.tatielou.co.uk/oranges/", "https://www.tatielou.co.uk/pears/"]
+from_list = ["apple", "apples", "appl", "recal", "house", "similarity"]
+to_list = ["apple", "apples", "mouse"]
+model = PolyFuzz("EditDistance")
+model.match(from_list, to_list)
+# Auto map by Similarity scores
+model.get_matches()
